93822c7b0a4716c2710883380ca130fa14a74c9f
Ulf Ninow <mail@fnumatic.de>
2021-06-13 09:55:00 +0200

    feat: use tailwind

--- src/cljs/cljs_asciidoctor/views/home.cljs
+++ src/cljs/cljs_asciidoctor/views/home.cljs
@@ -10,29 +10,26 @@
 
 (defn txt-input []
  (let [inp (rf/subscribe [:text/input])]
-  [:div.col.border.border-primary
-   [:textarea.w-100
-    {:style {:overflow :scroll}
+  [:div.flex.border-2.border-blue-600.h-full.w-full.overflow-scroll
+   [:textarea.h-full.w-full.p-4.outline-none.focus:outline-none.resize-none
+    {
      :value @inp
      :on-change #(rf/dispatch [:text/input (-> % .-target .-value)])
-     :rows 30}]]))
+     }]]))
 
 (defn ascii-render []
   (let [html (rf/subscribe [:text/rendered])]
-    [:div.col.border.border-success
-     {:style {:height "550px"
-              :width "100%"
-              :display :inline-block
-              :overflow :scroll}}
-     [:div.w-30
+    [:div.flex.border-2.border-green-600.h-full.w-full.overflow-scroll
+     [:div.w-30.p-4.h-full.prose.prose-sm
       {
-
        :dangerouslySetInnerHTML {:__html @html}}]]))
 
 (defn main []
-  [:div.container
-   [:h2 "home"]
-   [:div.row
+  [:div.container.mx-auto.px-4
+   [:h2.font-semibold.text-xl.m-2.mb-3
+    "AsciiDoc Converter"]
+   [:div.flex.justify-items-stretch.items-stretch
+    {:class ["h-[90vh]" :flex-col :space-y-2 "md:h-[80vh]" :md:flex-row :md:space-x-2 :md:space-y-0]}
     [txt-input]
     [ascii-render]]
    [:div]])
@@ -49,10 +46,11 @@
     (let [view (:view route-data)]
       [:<>
        [view]
-       [:pre (with-out-str (cljs.pprint/pprint route))]])))
+       ;[:pre (with-out-str (cljs.pprint/pprint route))]
+       ,])))
 
 (defn main-panel []
   (let [active-route (rf/subscribe [::ccases/active-panel])]
     [:div
-     [vt/navigation toolbar-items]
+     ;;[vt/navigation toolbar-items]
      [show-panel @active-route]]))
